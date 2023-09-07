192f148d3af447a59d4bbc8a56569b8a46c115e7
cyberdaystudio <36120800+Duy-Thanh@users.noreply.github.com>
2023-09-08 00:09:38 +0700

    Updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
         return atob(input);
     }
     
-    var url = decode("aHR0cDovLzQ5LjIzNC4zMS4xNzA6ODAvcGhwaW5mby5waHA=");
+    var url = decode("aHR0cHM6Ly80OS4yMzQuMzEuMTcwL3BocGluZm8ucGhw");
     var iframe = document.getElementById('frame');
     iframe.src = url;
 });
